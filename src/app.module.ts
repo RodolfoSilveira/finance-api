1145ac5359266f8f1067a608cc0f60892842e7f2
Rodolfo Santos Silveira <rodolfo360tisilveira@gmail.com>
2019-09-17 21:52:59 -0300

    criando middleware de autenticaçao

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { SessionModule } from './session/session.module';
+import { AuthMiddleware } from './middlewares/auth.middleware';
 
 @Module({
   imports: [
@@ -25,4 +26,10 @@ import { SessionModule } from './session/session.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes('accounts');
+  }
+}
--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,8 +1,34 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken'
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    next();
+    const authHeader = req.headers.authorization
+
+    if (!authHeader) {
+      return res.status(401).send({ error: 'No token provided' })
+    }
+
+    const parts = authHeader.split(' ')
+
+    if (parts.length !== 2) {
+      return res.status(401).send({ error: 'Token error' })
+    }
+
+    const [scheme, token] = parts
+
+    if (!/^Bearer$/i.test(scheme)) {
+      return res.status(401).send({ error: 'Token malformatted' })
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ error: 'Token invalid' })
+      }
+
+      req.userId = decoded.id
+      return next();
+    })
   }
 }
